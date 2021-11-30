01002e4686e833215b3dcf0c41a273890a3b1e3c
pdhanoa <dhanoa.ne@gmail.com>
2021-11-30 02:36:32 -0600

    Updates, added comments

--- main.py
+++ main.py
@@ -22,23 +22,15 @@ def sex_tdr_corr(df):
     # print(df.corr())
     a = sns.catplot(x='tdr_paludisme_string', hue='sex', kind='count', data=df)
     plt.savefig('analysis.png')
-
-    # T-test to see if there's a significant difference between test results of female and male
-    gender_significance = stats.ttest_ind(female_values['tdr_paludisme_bool'], male_values['tdr_paludisme_bool'])
-    print("Statistical results", gender_significance)
-    print("Female average:")
-    print(female_values['tdr_paludisme_bool'].mean())
-    print("Male average:")
-    print(male_values['tdr_paludisme_bool'].mean())
-
     # Correlation between weight and positive test
     plt.clf()
 
 
 def age_tdr_corr(df):
-    positive_values  = df[df['tdr_paludisme_bool'] == 1]
-    negative_values = df[df['tdr_paludisme_bool'] == 0]
+    positive_values = df[df['tdr_paludisme_string'] == 'tdr+']
+    negative_values = df[df['tdr_paludisme_string'] == 'tdr-']
 
+    # Using t-test to see if there is a significant difference between the age of those with and without HIV
     age_significance = stats.ttest_ind(positive_values['age_in_months'], negative_values['age_in_months'])
     print("age significance values:", age_significance)
     sns.kdeplot(data=df, x='age_in_months', hue='tdr_paludisme_string')
@@ -46,11 +38,14 @@ def age_tdr_corr(df):
     plt.clf()
 
 
+
 def weight_tdr_corr(df):
+    # Calculating BMI for BMI correlation with malaria positivity rate
     df['BMI'] = df['weight'] * np.power(df['height']/1000, 2)
 
-    positive_values = df[df['tdr_paludisme_bool'] == 1]
-    negative_values = df[df['tdr_paludisme_bool'] == 0]
+    # Dividing positive values and negative values to observe any significant differences in weight, height, etc.
+    positive_values = df[df['tdr_paludisme_string'] == 'tdr+']
+    negative_values = df[df['tdr_paludisme_string'] == 'tdr-']
     weight_significance = stats.ttest_ind(positive_values['weight'], negative_values['weight'])
     print("weight significance values:", weight_significance)
     sns.kdeplot(data=df, x='weight', hue='tdr_paludisme_string')
@@ -70,33 +65,25 @@ def weight_tdr_corr(df):
     plt.savefig('bmi_cluster.png')
     plt.clf()
 
-
-
-
 def hiv_tdr_corr(df):
     # The chi_2 test seems to state that there's a significant difference between the groups. Unlikely HIV test seems
-    # more likely to provide positive TDR+ test results, but
+    # more likely to provide positive TDR+ test results, but other resources indicate that this is not true.
+
+    # Should look into why no HIV positive tests who up on the graph from what I have.
     chisqt = pd.crosstab(df['classification_vih'], df['tdr_paludisme_string'])
     vih_contingency = stats.chi2_contingency(chisqt)
     print("Significance between positive test and HIV positive test:", vih_contingency)
 
     b = sns.catplot(x='tdr_paludisme_string', hue='classification_vih', kind='count', data=df)
     plt.savefig('hiv_tdr_relationship.png')
-    print("We got to this point")
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     df = pd.read_csv('/Users/pdhanoa/Box Sync/NU-malaria-team/data/burkina_TdH/Base_code/ieda_2_who.csv')
     d = {'tdr+': True, 'tdr-': False}
-    df['tdr_paludisme_bool'] = df['tdr_paludisme_string'].map(d)
-    df = df[df['tdr_paludisme_bool'].notna()]
-    weight_tdr_corr(df)
+    df = df[df['tdr_paludisme_string'].notna()]
+    hiv_tdr_corr(df)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
